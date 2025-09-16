35f565c44c157607c258441ad2130c4a75bebbc1
SloaneSamele <120995022+SloaneSamele@users.noreply.github.com>
2025-09-16 09:56:33 -0400

    Added initial block letters

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -1,5 +1,13 @@
 public class Assignment1Program1 {
   public static void main(String[] args) {
+    System.out.println("  SSSS          SSSS  ");
+    System.out.println("SS    SS      SS    SS");
+    System.out.println("SSS           SSS     ");
+    System.out.println("  SSS           SSS  ");
+    System.out.println("    SSS           SSS  ");
+    System.out.println("      SS            SS");
+    System.out.println("SS     SS       SS     SS");
+    System.out.println("   SSS              SSS  ");
 
   }
 }
