c8cfa48db7fe70f88f96ef36821cdc03ceb01968
SloaneSamele <120995022+SloaneSamele@users.noreply.github.com>
2025-09-16 10:04:27 -0400

    Added variables

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 import java.util.Random;
 public class Assignment1Program1 {
   public static void main(String[] args) {
+    String message;
+    double temp;
+    int randomNum;
     System.out.println("  SSSS          SSSS  ");
     System.out.println("SS    SS      SS    SS");
     System.out.println("SSS           SSS     ");
