5ee69e2aa7aa1e57992a3d5d663b396552a48700
SloaneSamele <120995022+SloaneSamele@users.noreply.github.com>
2025-09-17 10:33:49 -0400

    Used StringBuilder to reverse message

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -12,6 +12,7 @@ public class Assignment1Program1 {
     
     Scanner input = new Scanner(System.in);
     Random generator = new Random();
+    StringBuilder builder;
 
     System.out.println("  SSSS          SSSS  ");
     System.out.println("SS    SS      SS    SS");
@@ -24,6 +25,10 @@ public class Assignment1Program1 {
 
     System.out.println("Enter a 5 character String:");
     message = input.nextLine();
+    message = message.substring(1,4);
+
+    builder = new StringBuilder(message);
+    message = builder.reverse().toString();
     System.out.println("Enter a temperture in Farhenheit:");
     temp = input.nextFloat();
     temp = CONVERSION_FACTOR *(temp - BASE);
