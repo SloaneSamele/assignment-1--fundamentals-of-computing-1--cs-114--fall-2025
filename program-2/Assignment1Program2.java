4dd6a0a1967d79e7565ca2ca4c7210fb75ac2490
SloaneSamele <120995022+SloaneSamele@users.noreply.github.com>
2025-09-18 09:47:14 -0400

    Fixed spelling

--- program-2/Assignment1Program2.java
+++ program-2/Assignment1Program2.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Assignment1Program2 {
   public static void main(String[] args) {
-    int base, maxium, num, num2, outputNum;
+    int base, maximum, num, num2, outputNum;
 
     Scanner input = new Scanner(System.in);
     
@@ -10,9 +10,9 @@ public class Assignment1Program2 {
     System.out.println("Please enter a number between 2-9:");
     base = input.nextInt();
     num = base - 1;
-    maxium = ((num* (base * base * base)) + (num * (base * base)) + (num * base) + (num));
+    maximum = ((num* (base * base * base)) + (num * (base * base)) + (num * base) + (num));
     
-    System.out.println("The maxium base 10 number in base "+ base + " is "+ maxium + "\nPlease enter a number between 0 - " + maxium);
+    System.out.println("The maximum base 10 number in base "+ base + " is "+ maximum + "\nPlease enter a number between 0 - " + maximum);
     num = input.nextInt();
     
     num2 = num/base;
