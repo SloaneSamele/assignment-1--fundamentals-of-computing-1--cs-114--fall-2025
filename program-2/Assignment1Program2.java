3bcfbaf4c0799cb333b1665f4a8c592f72078648
SloaneSamele <120995022+SloaneSamele@users.noreply.github.com>
2025-09-18 09:46:16 -0400

    Removed variables

--- program-2/Assignment1Program2.java
+++ program-2/Assignment1Program2.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Assignment1Program2 {
   public static void main(String[] args) {
-    int base, maxium;
     int base, maxium, num, num2, outputNum;
 
     Scanner input = new Scanner(System.in);
