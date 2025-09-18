522414e190c4c7e1549548d3a13c4c50776ff67c
SloaneSamele <120995022+SloaneSamele@users.noreply.github.com>
2025-09-18 09:44:47 -0400

    Calculated the maximum and took input

--- program-2/Assignment1Program2.java
+++ program-2/Assignment1Program2.java
@@ -6,9 +6,14 @@ public class Assignment1Program2 {
     int base, maxium, num, num2, outputNum;
 
     Scanner input = new Scanner(System.in);
+    
     System.out.println("Base Conversion Calculator");
     System.out.println("Please enter a number between 2-9:");
     base = input.nextInt();
+    num = base - 1;
+    maxium = ((num* (base * base * base)) + (num * (base * base)) + (num * base) + (num));
+    System.out.println("The maxium base 10 number in base "+ base + " is "+ maxium + "\nPlease enter a number between 0 - " + maxium);
+    num = input.nextInt();
     input.close();
   }
 }
