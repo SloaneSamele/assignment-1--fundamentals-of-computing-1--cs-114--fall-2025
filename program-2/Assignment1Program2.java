117a0845e698ce1f0ea685524fe211c257c150d0
SloaneSamele <120995022+SloaneSamele@users.noreply.github.com>
2025-09-18 08:00:41 -0400

    Added input of a base

--- program-2/Assignment1Program2.java
+++ program-2/Assignment1Program2.java
@@ -1,8 +1,13 @@
 import java.util.Scanner;
+
 public class Assignment1Program2 {
   public static void main(String[] args) {
+    int base, maxium;
 
     Scanner input = new Scanner(System.in);
+    System.out.println("Base Conversion Calculator");
+    System.out.println("Please enter a number between 2-9:");
+    base = input.nextInt();
     input.close();
   }
 }
